fe001a79cf61482ceea2495e68ea850665235acf
Conor Ryan <37616617+conorjcryan@users.noreply.github.com>
2019-03-01 20:20:53 +0000

    MQTT based local hub
    will add external functionality again later

--- Hub.py
+++ Hub.py
@@ -1,123 +1,155 @@
 import socket
 import time
-import thread
+import _thread
 import datetime
+import Adafruit_DHT
+import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+from rpi_rf import RFDevice
 
-#devices and values
-MainHeatStatus = 0
-BedHeatStatus = 0
-BlanketStatus = 0
-LightStatus = 0
-MainTemp = 0
-MainHumid = 0
-BedTemp = 0
-BedHumid = 0
-Source = '0'
+MQTT_SERVER = "localhost"
+TEMP_PATH = "Temp"
+HUMID_PATH = "Humid"
+HEAT_PATH = "Heat"
+LIGHT_PATH = "Light"
+BLANKET_PATH = "Blanket"
+TOGGLE_PATH = "Toggle"
 
-def listen():
+Temp = 0
+Humid = 0
+Heat = 1
+Blanket = 0
+Light = 1
 
-        global BedTemp
-        global BedHumid
-        global Source
+def ControlSockets():
+    
+    global Light
+    global Heat
+    global Blanket
+    
+    Radio = RFDevice(17)
+    Radio.enable_tx()
 
-        Listen = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        Listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    HeatOn = 5330227
+    HeatOff = 5330236
+    LightOn = 5330691
+    LightOff = 5330700
+    BlanketOn = 5330371
+    BlanketOff = 5330380
+    UnusedOn = 5332227
+    UnusedOff = 5332236
+    
+    PIN = 189
+    
+    while True:
+        
+        if  Heat == 0:            
+            Radio.tx_code(HeatOff, 1, PIN)
+        
+        elif Heat == 1:           
+            Radio.tx_code(HeatOn, 1, PIN)
+        
+        time.sleep(1)
+        
+        if  Light == 0:            
+            Radio.tx_code(LightOff, 1, PIN)
+        
+        elif Light == 1:            
+            Radio.tx_code(LightOn, 1, PIN)
+            
+        time.sleep(1)
+            
+        if  Blanket == 0:           
+            Radio.tx_code(BlanketOff, 1, PIN)
+        
+        elif Blanket == 1:            
+            Radio.tx_code(BlanketOn, 1, PIN)
+        
+        time.sleep(1)
 
-        #bind it to port 9998 and accept data from all IP addresse
-        Listen.bind(("0.0.0.0",9998))
+def SensorReadings():
+    
+    global Temp
+    global Humid
+    
+    #temp values for tracking updates
+    CTemp = Temp
+    CHumid = Humid
+    
+    while True:
+        
+        #attempt to get data from the sensor module
+        X, Y = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22,27)
+        #convert to integers
+        T = int(Y)
+        H = int(X)
+        
+        #If between 0 and 99 and the first reading, or between 0 and 99 and the reading has changed but not more than 25%
+        if H >= 0 and H < 100 and CHumid == 0 or H >= 0 and H < 100 and H < CHumid*1.25 and H > CHumid*0.75:
+             
+            #update relevent values 
+            Humid = H
+            CHumid = H
+            
+            #print status message indicating an update
+            print("Living Room Humidity at " + str(H) + "%")            
+            publish.single(HUMID_PATH, str(H), hostname=MQTT_SERVER)
+         
+        #If between 0 and 99 and the first reading, or between 0 and 99 and the reading has changed but not more than 25%
+        if T >= 0 and T < 100 and CTemp == 0 or T >= 0 and T < 100 and T < CTemp*1.25 and T > CTemp*0.75:
 
-        #create a loop
-        while True:
+            #update relevent values 
+            Temp = T
+            CTemp = T
+            
+            #print status message indicating an update
+            print("Living Room is " + str(T) + " °C")           
+            publish.single(TEMP_PATH, str(T), hostname=MQTT_SERVER)            
+            
+        time.sleep(10)
+            
+def PublishDevices():
+    
+    global Heat
+    global Light
+    global Blanket
+    
+    while True:       
+        publish.single(HEAT_PATH, str(Heat), hostname=MQTT_SERVER)
+        publish.single(LIGHT_PATH, str(Light), hostname=MQTT_SERVER)
+        publish.single(BLANKET_PATH, str(Blanket), hostname=MQTT_SERVER)
+        time.sleep(1)
+           
+#start listening and sensor threads
+_thread.start_new_thread(ControlSockets, ())
+_thread.start_new_thread(PublishDevices, ())
+_thread.start_new_thread(SensorReadings, ())
 
-                #try to get data from the socket
-                data, address = Listen.recvfrom(7)
+#startup complete status message
+print ("Online at",datetime.datetime.now().strftime("%I:%M:%S %p"))
 
-                #check the message recieved
-                if data.startswith('1'):
-                        togglemain()
-                if data.startswith('2'):
-                        togglebed()
-                if data.startswith('3'):
-                        toggleblanket()
-                if data.startswith('4'):
-                        togglelight()
-                if data.startswith('5'):
-                        x,temp,humid = data.split(",")
-                        BedTemp = temp
-                        BedHumid = humid
-                if data == '9':
-                        Source = address
-                        thread.start_new_thread(updatedevice, ())
-
-def updatedevice():
-
-        global BedHeatStatus
-        global MainHeatStatus
-        global BlanketStatus
-        global LightStatus
-        global MainTemp
-        global MainHumid
-        global BedTemp
-        global BedHumid
-        global Source
-
-        Send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        Send.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-        Status = str(MainHeatStatus) + ',' + str(BedHeatStatus) + ',' + str(BlanketStatus) + ',' + str(LightStatus) + ',' + str(MainTemp).zfill(2) + ',' + str(MainHumi$
-        Send.sendto(Status.encode(),Source)
-        print('Sent device data' + Status + ' To: ' + str(Source))
-
-def togglemain():
-
-        global MainHeatStatus
-
-        if MainHeatStatus == 1:
-
-                MainHeatStatus = 0
-                print("Switched off the living room heater.")
-
-                MainHeatStatus = 1
-                print("Switched on the living room heater.")
-
-def togglebed():
-
-        global BedHeatStatus
-
-        if BedHeatStatus == 1:
-
-                BedHeatStatus = 0
-                print("Switched off the bedroom heater.")
-
-        elif BedHeatStatus == 0:
-
-                BedHeatStatus = 1
-                print("Switched on the bedroom heater.")
-
-def toggleblanket():
-
-        global BlanketStatus
-
-        if BlanketStatus == 1:
-
-                BlanketStatus = 0
-                print("Switched off the electric blanket.")
-
-        elif BlanketStatus == 0:
-                BlanketStatus = 1
-                print("Switched on the electric blanket.")
-
-def togglelight():
-
-        global LightStatus
-
-        if LightStatus == 1:
-
-                LightStatus = 0
-                print("Switched off the lamp.")
-
-thread.start_new_thread(listen,())
-
-while True:
-
-        time.sleep(600)
+def on_connect(client, userdata, flags, rc):
+     
+    client.subscribe(TOGGLE_PATH)
+    
+def on_message(client, userdata, msg):
+    
+    print(str(msg.payload))
+    Request = str(msg.payload)
+    
+    if Request == 1:        
+        ToggleHeat()
+        
+    if Request == 1:        
+        ToggleLight()
+        
+    if Request == 1:        
+        ToggleBlanket()
+        
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(MQTT_SERVER, 1883, 60)
+client.loop_forever()
